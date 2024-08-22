61407ff36cd1544e0ddde1006ada49b4d3252323
safi <safiou102003@gmail.com>
2024-08-22 11:34:06 -0700

    faet:local storage

--- utils/stor.js
+++ utils/stor.js
@@ -6,13 +6,20 @@ const useDifficulty = create(
     difficulty: "",
     addDifficulty: (difficultyLevel) =>
       set((state) => ({ difficulty: difficultyLevel })),
-  }))
+  })),
+  {
+    name: 'difficulty-storage',
+  }
+  
 );
 const useFeature = create(
   persist((set) => ({
     feature: "",
     addFeature: (featureName) => set((state) => ({ feature: featureName })),
-  }))
+  })),
+  {
+    name: 'feature-storage',
+  }
 );
 export { useDifficulty };
 export { useFeature };
