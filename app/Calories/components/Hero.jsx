20bdc8f8a3270f9f0fc41edb4ed06ed704426cfe
safi <safiou102003@gmail.com>
2024-08-23 11:51:13 -0700

    feat:form

--- app/Calories/components/Hero.jsx
+++ app/Calories/components/Hero.jsx
@@ -1,5 +1,7 @@
+"use client";
 import React from "react";
 import getCalorie from "../actions/getCalorie";
+import { useState } from "react";
 export default async function Hero() {
   function calculateBMR(gender, weight, height, age) {
     let BMR;
@@ -47,13 +49,53 @@ export default async function Hero() {
   const height = 175; // in cm
   const age = 25; // in years
   const activityLevel = "moderate";
-
+  const goal = "lose"
   const BMR = calculateBMR(gender, weight, height, age);
   const dailyCalories = calculateDailyCalories(BMR, activityLevel, goal);
 
   return (
     <div>
-    
+      <form action="">
+        <div class="max-w-sm space-y-3">
+          <label htmlFor="">gender</label>
+          <input
+            type="text"
+            className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+            placeholder="This is placeholder"
+          />
+            <label htmlFor="">weight</label>
+             <input
+            type="text"
+            className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+            placeholder="This is placeholder"
+          />
+           <label htmlFor="">height</label>
+             <input
+            type="text"
+            className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+            placeholder="
+            This is placeholder"
+          />
+          <label htmlFor="">age</label>
+             <input
+            type="text"
+            className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+            placeholder="This is placeholder"
+          />
+          <label htmlFor="">activityLevel</label>
+             <input
+            type="text"
+            className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+            placeholder="This is placeholder"
+          />
+           <label htmlFor="">goal</label>
+             <input
+            type="text"
+            className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+            placeholder="This is placeholder"
+          />
+        </div>
+      </form>
     </div>
   );
 }
