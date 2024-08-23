2129a32f74d387f6431ba72bc7b2fd5bfbee028e
safi <safiou102003@gmail.com>
2024-08-23 12:48:39 -0700

    fix:ui form

--- app/Calories/components/Hero.jsx
+++ app/Calories/components/Hero.jsx
@@ -2,7 +2,22 @@
 import React from "react";
 import getCalorie from "../actions/getCalorie";
 import { useState } from "react";
-export default async function Hero() {
+export default function Hero() {
+  const [formData, setFormData] = useState({
+    gender: "",
+    weight: "",
+    height: "",
+    age: "",
+    activityLevel: "",
+    goal: "",
+  });
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  }
   function calculateBMR(gender, weight, height, age) {
     let BMR;
 
@@ -44,56 +59,95 @@ export default async function Hero() {
     }
     return dailyCalories;
   }
-  const gender = "male"; // or "female"
-  const weight = 70; // in kg
-  const height = 175; // in cm
-  const age = 25; // in years
-  const activityLevel = "moderate";
-  const al = "lose"
+  const gender = formData.gender; // or "female"
+  const weight = formData.weight; // in kg
+  const height = formData.height; // in cm
+  const age = formData.age; // in years
+  const activityLevel = formData.activityLevel;
+  const goal = formData.goal;
   const BMR = calculateBMR(gender, weight, height, age);
   const dailyCalories = calculateDailyCalories(BMR, activityLevel, goal);
 
   return (
-    <div>
+    <div className="flex justify-center items-center w-full flex-col ">
       <form action="">
-        <div class="max-w-sm space-y-3">
-          <label htmlFor="">gender</label>
+        <div class="flex flex-col gap-5">
+          <label htmlFor="">Gender:</label>
+          <select
+            className="py-2 px-3 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+            name="gender"
+            value={formData.gender}
+            onChange={handleChange}
+          >
+            <option value="">Select Gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+
+          <label htmlFor="">Weight (kg):</label>
           <input
             type="text"
-            className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+            className="py-3 px-4 block w-full border-gray-200 border rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
             placeholder="This is placeholder"
+            name="weight"
+            value={formData.weight}
+            onChange={handleChange}
           />
-            <label htmlFor="">weight</label>
-             <input
+          <label htmlFor="">Height (cm):</label>
+          <input
             type="text"
-            className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-            placeholder="This is placeholder"
-          />
-           <label htmlFor="">height</label>
-             <input
-            type="text"
-            className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+            className="py-3 px-4 block w-full border-gray-200 border rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
             placeholder="
             This is placeholder"
+            name="height"
+            value={formData.height}
+            onChange={handleChange}
           />
-          <label htmlFor="">age</label>
-             <input
+          <label htmlFor="">Age:</label>
+          <input
             type="text"
-            className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+            className="py-3 px-4 block w-full rounded-lg border border-gray-200 text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
             placeholder="This is placeholder"
+            name="age"
+            value={formData.age}
+            onChange={handleChange}
           />
-          <label htmlFor="">activityLevel</label>
-             <input
-            type="text"
-            className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-            placeholder="This is placeholder"
-          />
-           <label htmlFor="">goal</label>
-             <input
-            type="text"
-            className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-            placeholder="This is placeholder"
-          />
+          <label htmlFor=""> Activity Level:</label>
+
+          <select
+            className="py-2 px-3 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+            name="activityLevel"
+            value={formData.activityLevel}
+            onChange={handleChange}
+          >
+            <option value="">Select Activity Level</option>
+            <option value="sedentary">Sedentary</option>
+            <option value="light">Light</option>
+            <option value="moderate">Moderate</option>
+            <option value="active">Active</option>
+            <option value="very active">Very Active</option>
+          </select>
+
+          <label htmlFor="">Goal:</label>
+
+          <select
+            className="py-2 px-3 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
+            name="goal"
+            value={formData.goal}
+            onChange={handleChange}
+          >
+            <option value="">Select Goal</option>
+            <option value="lose">Lose Weight</option>
+            <option value="maintain">Maintain Weight</option>
+            <option value="gain">Gain Weight</option>
+          </select>
+
+          <button
+            type="button"
+            className="flex justify-center  py-3 px-4  items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+          >
+            calculate
+          </button>
         </div>
       </form>
     </div>
