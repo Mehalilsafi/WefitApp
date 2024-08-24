8a1089cf86290dad3b99187e33ebeb2c4948edcc
safi <safiou102003@gmail.com>
2024-08-24 13:11:14 -0700

    fix: grid

--- app/Meals/components/Hero.jsx
+++ app/Meals/components/Hero.jsx
@@ -15,7 +15,7 @@ export default function Hero() {
   useEffect(() => {
     const fetchMeals = async () => {
       try {
-        const response = await meals();
+        //const response = await meals();
         setData(response);
       } catch (err) {
         throw new err("error in Hero page ");
@@ -38,20 +38,29 @@ export default function Hero() {
     setSelectedChoice(choice);
   }
   return (
-    <div>
-      <div>
+    <div className="grid grid-cols-4 bg-slate-300 m-7">
+      <div className="flex flex-col gap-3 col-span-4 md:col-span-1  bg-slate-400 p-4">
+        <p>Choose one or more methods</p>
         {choiceList.map((choice) => (
           <p key={choice.label} onClick={() => handleClick(choice.label)}>
             {choice.label}
           </p>
         ))}
       </div>
-      <div>
+      <div className="col-span-4 md:col-span-2 bg-slate-200 p-4">
         {choiceList
           .filter((choice) => choice.label === selectedChoice)
           .map((choice) => (
             <div key={choice.label}>{choice.component}</div>
           ))}
+      </div>
+      <div className="col-span-4 md:col-span-1  bg-slate-600 p-4">
+        <button
+          type="button"
+          className="py-3 px-4 m-auto mt-5 flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-800 text-gray-800 hover:border-gray-500 hover:text-gray-500 focus:outline-none focus:border-gray-500 focus:text-gray-500 disabled:opacity-50 disabled:pointer-events-none dark:border-white dark:text-white dark:hover:text-neutral-300 dark:hover:border-neutral-300  "
+        >
+          Search
+        </button>
       </div>
     </div>
   );
