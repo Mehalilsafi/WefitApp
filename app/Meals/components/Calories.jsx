b68f56785f80624709f3b0e5344eb2349308318e
safi <safiou102003@gmail.com>
2024-08-27 02:55:35 -0700

    feat:calories

--- app/Meals/components/Calories.jsx
+++ app/Meals/components/Calories.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+"use client";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faCheck } from "@fortawesome/free-solid-svg-icons";
 
 export default function Calories() {
   return (
-    <div>Calories</div>
-  )
+    <div className="">
+      <p className="mb-4 border-b border-solid border-accent text-accent">
+        Input maximum desired calories per serving. For food database results
+        default serving is 100g.
+      </p>
+      <div className="flex justify-start items-center gap-3">
+        <input type="text"  className="w-32 h-10 rounded"/>
+        <p className="font-medium text-lg">kcal</p>
+        <FontAwesomeIcon
+          icon={faPlus}
+          className="p-1 bg-four text-primary rounded-full font-medium"
+        />
+      </div>
+    </div>
+  );
 }
--- app/Meals/components/Diets.jsx
+++ app/Meals/components/Diets.jsx
@@ -31,18 +31,18 @@ export default function Diets() {
   }
 
   return (
-     <div className="flex gap-4 overflow-hidden ">
+     <div className="grid grid-cols-3 items-center gap-4  ">
       {data.map((item, index) => (
         <div
           key={index}
-          className="flex gap-1 items-center bg-accent rounded-xl p-1 h-16"
+          className="flex gap-1 justify-center items-center bg-accent rounded-full p-1 h-9  col-span-1"
           onClick={() => handleIconClick(item)}
         >
           <FontAwesomeIcon
             icon={selectedItems.includes(item) ? faCheck : faPlus}
             className="p-1 bg-four text-primary rounded-full font-medium"
           />
-          <p className="text-gray-400">{item}</p>
+          <p className="text-gray-400 w-full">{item}</p>
         </div>
       ))}
     </div>
