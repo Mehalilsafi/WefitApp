014a8e3e14a50f121465b125385c8a59abf0e8f2
safi <safiou102003@gmail.com>
2024-08-27 03:19:52 -0700

    feat:calories toggel

--- app/Meals/components/Calories.jsx
+++ app/Meals/components/Calories.jsx
@@ -4,6 +4,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faCheck } from "@fortawesome/free-solid-svg-icons";
 
 export default function Calories() {
+  const [selected, setSelected] = useState(true);
+  function handleClick() {
+    setSelected((prev) => !prev);
+  }
   return (
     <div className="">
       <p className="mb-4 border-b border-solid border-accent text-accent">
@@ -11,11 +15,16 @@ export default function Calories() {
         default serving is 100g.
       </p>
       <div className="flex justify-start items-center gap-3">
-        <input type="text"  className="w-32 h-10 rounded"/>
+        <input type="text" className="w-32 h-10 rounded" />
         <p className="font-medium text-lg">kcal</p>
         <FontAwesomeIcon
-          icon={faPlus}
-          className="p-1 bg-four text-primary rounded-full font-medium"
+          icon={selected ? faPlus : faCheck}
+          className={
+            selected
+              ? "p-1 bg-four text-primary rounded-full font-medium"
+              : "p-1 bg-four text-secondary rounded-full font-medium"
+          }
+          onClick={handleClick}
         />
       </div>
     </div>
