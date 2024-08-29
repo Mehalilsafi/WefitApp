e098f444da832e30102b47a0244c0ad259bb4593
safi <safiou102003@gmail.com>
2024-08-28 17:21:23 -0700

    feat:some changes

--- app/Meals/page.jsx
+++ app/Meals/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import Hero from './components/Hero'
-export default function page() {
+ export default function page() {
   return (
     <div>
       <Hero/>
