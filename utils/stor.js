e7c010d249111e8a006bfcc6e9f350e93fb3cb45
safi <safiou102003@gmail.com>
2024-09-02 07:56:47 -0700

    feat:other methods

--- utils/stor.js
+++ utils/stor.js
@@ -42,12 +42,50 @@ const useStore = create(
               : [...state.choice.diets, item],
           },
         })),
+
+      addAllergies: (item) =>
+        set((state) => ({
+          choice: {
+            ...state.choice,
+            allergies: state.choice.allergies.includes(item)
+              ? state.choice.allergies.filter((i) => i !== item)
+              : [...state.choice.allergies, item],
+          },
+        })),
+      addSearch: (item) =>
+        set((state) => ({
+          choice: {
+            ...state.choice,
+            search: state.choice.search.includes(item)
+              ? state.choice.search.filter((i) => i !== item)
+              : [...state.choice.search, item],
+          },
+        })),
+      addCalories: (item) =>
+        set((state) => ({
+          choice: {
+            ...state.choice,
+            calories: state.choice.calories.includes(item)
+              ? state.choice.calories.filter((i) => i !== item)
+              : [...state.choice.calories, item],
+          },
+        })),
+      addNutrients: (item) =>
+        set((state) => ({
+          choice: {
+            ...state.choice,
+            nutrients: state.choice.nutrients.includes(item)
+              ? state.choice.nutrients.filter((i) => i !== item)
+              : [...state.choice.nutrients, item],
+          },
+        })),
     }),
     {
       name: "choice-storage",
     }
   )
 );
+
 export { useStore };
 export { useDifficulty };
 export { useFeature };
